2b663f3f9b45b5b266b75adcfda8746ae63d1c35
MihneaTs1 <103445201+MihneaTs1@users.noreply.github.com>
2024-09-10 18:10:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
-#define make_your_code_200_times_faster_no_clickbait ios_base::sync_with_stdio(false);\
-    /* "You shouldn't see this." */                             cin.tie(nullptr);\
-    /*                        - MihneaTheMogger */              cout.tie(nullptr);
+#define FastIO ios_base::sync_with_stdio(false);\
+                               cin.tie(nullptr);\
+                 			  cout.tie(nullptr);
 using namespace std;
 
-/// What you'll be actually using
-#define int int64_t        /// <--|
-#define YES cout<<"YES"    ///    |
-#define YESn cout<<"YES\n" /// <--|
-#define NO cout<<"NO"      ///    |
-#define NOn cout<<"NO\n"   /// <--|                                                                                                                                                                                                                      As you see, I don't use that many shortcuts. Very mindful. Very demure.
-/* just to emphasize */    ///    |
-#define MORE_TESTS 1       /// <--|-- TAKE NOTES GUYS
-#define FASTER_CODE 1      /// <--|-- HERE TOO
-int32_t TESTCASE_COUNT = 1, TESTCASE;
+#define int int64_t 
+#define YES cout<<"YES"    
+#define YESn cout<<"YES\n" 
+#define NO cout<<"NO"     
+#define NOn cout<<"NO\n"       
 
-/// START OF CODE
+#define MORE_TESTS 1      
+#define FASTIO 1     
+int32_t TESTCASE_COUNT = 1, TESTCASE;
 
 void Solve ()
 {
@@ -27,23 +24,19 @@ void Solve ()
 
 */
 
-/// END OF CODE
-
 int32_t main ()
 {
-    #if FASTER_CODE == 1
-    make_your_code_200_times_faster_no_clickbait; // if you disable it, it will disable your CF rating
+    #if FASTIO == 1
+    FastIO;
     #endif
     #if MORE_TESTS == 1
     cin >> TESTCASE_COUNT;
     #endif
-    // If you are here, stop it. Get some help.
-    for (TESTCASE = 1; TESTCASE <= TESTCASE_COUNT; TESTCASE ++) /// for debug purposes
+    for (TESTCASE = 1; TESTCASE <= TESTCASE_COUNT; TESTCASE ++)
         Solve ();
 }
 
 /*
-
 ___  ___          _        ______         ___  ____ _                    _____ _         ___  ___
 |  \/  |         | |       | ___ \        |  \/  (_) |                  |_   _| |        |  \/  |
 | .  . | __ _  __| | ___   | |_/ /_   _   | .  . |_| |__  _ __   ___  __ _| | | |__   ___| .  . | ___   __ _  __ _  ___ _ __
@@ -52,6 +45,4 @@ ___  ___          _        ______         ___  ____ _                    _____ _
 \_|  |_/\__,_|\__,_|\___|  \____/ \__, |  \_|  |_/_|_| |_|_| |_|\___|\__,_\_/ |_| |_|\___\_|  |_/\___/ \__, |\__, |\___|_|
                                    __/ |                                                                __/ | __/ |
                                   |___/                                                                |___/ |___/
-Happy coding! :)))))
-
 */
